9b3efabedda2a25e17cd6cc914b151c04728e637
Matthew Fincher <matthewfincher@gmail.com>
2016-10-07 00:51:06 -0400

    update depencencies

---
+++ modules/landscapes/client/config/landscapes.client.menus.js
@@ -0,0 +1,17 @@
+(function () {
+  'use strict';
+
+  angular
+    .module('landscapes')
+    .run(menuConfig);
+
+  menuConfig.$inject = ['menuService'];
+
+  function menuConfig(menuService) {
+    // Set top bar menu items
+    menuService.addMenuItem('topbar', {
+      title: 'Landscapes',
+      state: 'landscapes.list'
+    });
+  }
+}());
