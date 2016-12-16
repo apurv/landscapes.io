2bb686515b77a078ef138af7b39a7fde7d1ef821
apurv <apurv@Apurvs-MacBook-Pro.local>
2016-12-16 00:14:17 -0500

    added prod build capability

--- src/app/redux/store/configureStore.prod.js
+++ src/app/redux/store/configureStore.prod.js
@@ -1,40 +1,28 @@
-import {
-  createStore,
-  applyMiddleware,
-  compose,
-  combineReducers
-}                         from 'redux';
-import createLogger       from 'redux-logger';
-import thunkMiddleware    from 'redux-thunk';
-import * as reducers      from '../reducers';
-import { routerReducer }  from 'react-router-redux';
-import { apolloClient }   from './services/apollo';
+import { createStore, applyMiddleware, compose, combineReducers } from 'redux'
+import createLogger from 'redux-logger'
+import thunkMiddleware from 'redux-thunk'
+import * as reducers from '../modules/reducers'
+import { routerReducer } from 'react-router-redux'
+import { apolloClient } from '../../services/apollo'
 
-const loggerMiddleware = createLogger({
-  level     : 'info',
-  collapsed : true
-});
+const loggerMiddleware = createLogger({ level: 'info', collapsed: true })
 
 // createStore : enhancer
-const enhancer = compose(
-  applyMiddleware(
-    thunkMiddleware,
-    apolloClient.middleware(), // apollo middleware
-    loggerMiddleware
-  ) // logger after thunk to avoid undefined actions
-);
+const enhancer = compose(applyMiddleware(thunkMiddleware, apolloClient.middleware(), // apollo middleware
+        loggerMiddleware) // logger after thunk to avoid undefined actions
+)
 
 // combine reducers
 const reducer = combineReducers({
-  ...reducers,
-  apollo: apolloClient.reducer(), // apollo reducer
-  routing: routerReducer
-});
+    ...reducers,
+    apollo: apolloClient.reducer(), // apollo reducer
+    routing: routerReducer
+})
 
 // export default =  "redux store"
 export default function configureStore(initialState) {
-  return createStore(reducer, initialState, enhancer);
+    return createStore(reducer, initialState, enhancer)
 }
 
 // export "apollo client"
-export const client = apolloClient;
+export const client = apolloClient
