b7e1159e4b8ac8e0059cc7a6554429795592f273
apurv <apurv@Apurvs-MacBook-Pro.local>
2017-01-26 10:03:44 -0500

    Fixed conflicts

--- app/views/index.js
+++ app/views/index.js
@@ -1,25 +1,16 @@
 // non protected views:
-<<<<<<< Updated upstream
-import Landscapes        from './landscapes/Landscapes';
-import Home         from './home/Home';
-import PageNotFound from './pageNotFound/PageNotFound';
-import Login        from './login/Login';
-import Register     from './register/Register';
-import Password     from './password/Password';
-=======
+import Home         from './home/Home'
+import PageNotFound from './pageNotFound/PageNotFound'
+import Login        from './login/Login'
+import Register     from './register/Register'
+import Password     from './password/Password'
+// protected views:
+import Protected    from './protected/Protected'
 import CreateLandscape        from './landscapes/CreateLandscape'
 import EditLandscape        from './landscapes/EditLandscape'
 import LandscapeDetails        from './landscapes/LandscapeDetails'
 import Landscapes        from './landscapes/Landscapes'
 import Deployments        from './deployments/Deployments'
-import Home         from './home/Home'
-import PageNotFound from './pageNotFound/PageNotFound'
-import Login        from './login/Login'
-import Register     from './register/Register'
->>>>>>> Stashed changes
-// protected views:
-import Protected    from './protected/Protected'
-
 
 export {
   // non protected views:
